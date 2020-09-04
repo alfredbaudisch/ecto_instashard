32edb96460f239419e967edea8b28cb6c0bcc4e3
Alfred Reinold Baudisch <alfred.r.baudisch@gmail.com>
2020-09-04 14:57:50 +0200

    Bump hex version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Ecto.InstaShard.Mixfile do
 
   def project do
     [app: :ecto_instashard,
-     version: "0.2.1",
+     version: "0.3.0",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
@@ -32,7 +32,7 @@ defmodule Ecto.InstaShard.Mixfile do
   defp deps do
     [{:postgrex, "~> 0.13"},
      {:ecto, "~> 2.2"},
-     {:ex_doc, "~> 0.11.0", only: :dev},
+     {:ex_doc, "~> 0.19.0", only: :dev},
      {:earmark, ">= 0.0.0"}]
   end
 
